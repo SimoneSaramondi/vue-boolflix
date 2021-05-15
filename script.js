9537e8d1b1745381c893cd0771e42b7d29162338
SimoneSaramondi <simo98sara@gmail.com>
2021-05-15 09:38:49 +0200

    Milestone 2,3,4

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var app = new Vue ({
     el: '#app',
+    /*
     data: {
       arrayMovies: [],
-      thisSearch: ''
+      thisSearch: '',
     },
   
     methods: {
+      //Ricerca di un film all'interno del sito
       searchFunction: function() {  
         
         axios
@@ -13,18 +15,75 @@ var app = new Vue ({
           { params:
             {
               api_key: '2cd3ef7f0cd27a179b24f4410ce0e944',
-              query: this.thisSearch              
+              query: this.thisSearch,          
             }
           }
         )
   
         .then(risposta => {
-          this.arrayMovies = risposta.data.results;       
+          this.arrayMovies = risposta.data.results;   
   
           this.thisSearch = '';          
         });  
       },
   
+    },*/
+    data: {
+      arrayFilm: [],
+      arraySerieTv: [],
+      arrayFlags: ['de', 'en', 'es', 'fr', 'it', 'pt','ru', 'zh', 'ja'],
+      thisSearch: ''
     },
+    methods: {
+      searchFunction() {
+        this.searchFilm();
+        this.searchSerieTv();
+      },
+  
+      searchFilm(){
+        //Chiamata per movie:
+        axios
+        .get('https://api.themoviedb.org/3/search/movie',
+          { params:
+            {
+              api_key: '2cd3ef7f0cd27a179b24f4410ce0e944',
+              query: this.thisSearch,
+              language: 'it-IT'
+            }
+          }
+        )
+        .then(risposta => {
+          this.arrayFilm = risposta.data.results;
+        });
+      },
+  
+      searchSerieTv(){
+        //chiamata serie tv
+        axios
+        .get('https://api.themoviedb.org/3/search/tv',
+          { params:
+            {
+              api_key: '2cd3ef7f0cd27a179b24f4410ce0e944',
+              query: this.thisSearch,
+              language: 'it-IT'
+            }
+          }
+        )
+        .then(risposta => {
+          this.arraySerieTv = risposta.data.results;
+        });
+      },
+  
+      flagFunction: function(index) {
+        return this.arrayFlags.includes(index);
+      },
+      voteStarsFunct: function(movie) {
+        return Math.ceil( movie.vote_average / 2 );
+      },
+  
+      summonPoster: function(poster) {
+        return `https://image.tmdb.org/t/p/w342/${poster}`;
+      }
+    }
 
   })
